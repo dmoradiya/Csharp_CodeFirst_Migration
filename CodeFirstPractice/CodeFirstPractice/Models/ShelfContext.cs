84ca3b90a3e8fd813cb85be94253fa490ba56906
dhmec <dharmoradiya@gmail.com>
2020-10-30 14:26:08 -0600

    Add Connection, Model Creating method in ShelfContext.cs

--- CodeFirstPractice/CodeFirstPractice/Models/ShelfContext.cs
+++ CodeFirstPractice/CodeFirstPractice/Models/ShelfContext.cs
@@ -7,5 +7,31 @@ namespace CodeFirstPractice.Models
 {
     class ShelfContext : DbContext
     {
+        public virtual DbSet<Shelf> Shelves { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connection =
+                    "server=localhost;" +
+                    "port=3306;" +
+                    "user=root;" +
+                    "database=codefirst_practice;";
+
+                string version = "10.4.14-MariaDB";
+
+                optionsBuilder.UseMySql(connection, x => x.ServerVersion(version));
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Shelf>(entity =>
+            {
+                entity.Property(e => e.Name)
+                .HasCharSet("utf8mb4")
+                .HasCollation("utf8mb4_general_ci");
+            });
+        }
     }
 }

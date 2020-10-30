c190dc547ac507f259bff278c4a1a9f7e0e75425
dhmec <dharmoradiya@gmail.com>
2020-10-30 14:15:53 -0600

    Add Table name and  Column in Shelf.cs

--- CodeFirstPractice/CodeFirstPractice/Models/Shelf.cs
+++ CodeFirstPractice/CodeFirstPractice/Models/Shelf.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace CodeFirstPractice.Models
 {
+    [Table("shelf")]
     class Shelf
     {
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
     }
 }

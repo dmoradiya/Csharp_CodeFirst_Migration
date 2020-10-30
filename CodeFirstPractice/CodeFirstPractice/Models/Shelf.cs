79fd429d387f9e6bec2ec7b14bdb88925a8b80a2
dhmec <dharmoradiya@gmail.com>
2020-10-30 16:32:39 -0600

    Add foreignkey, virtual properties and constructors for references

--- CodeFirstPractice/CodeFirstPractice/Models/Shelf.cs
+++ CodeFirstPractice/CodeFirstPractice/Models/Shelf.cs
@@ -14,9 +14,16 @@ namespace CodeFirstPractice.Models
         [Column(TypeName = "int(10)")]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
-        
+
+        [Column(TypeName = "int(10)")]
+        public int ShelfMaterialID { get; set; }
+
         [Required]
         [Column(TypeName = "varchar(50)")]
         public string Name { get; set; }
+
+        [ForeignKey(nameof(ShelfMaterialID))]
+        [InverseProperty(nameof(Models.Shelf_Material.Shelves))]
+        public virtual Shelf_Material Shelf_Material { get; set; }
     }
 }
---
+++ CodeFirstPractice/CodeFirstPractice/Models/Shelf_Material.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CodeFirstPractice.Models
+{
+    [Table("shelf_material")]
+    class Shelf_Material
+    {
+        public Shelf_Material()
+        {
+            Shelves = new HashSet<Shelf>();
+        }
+
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(25)")]
+        public string MaterialName { get; set; }
+
+        [InverseProperty(nameof(Models.Shelf.Shelf_Material))]
+        public virtual ICollection<Shelf> Shelves { get; set; }
+    }
+}
